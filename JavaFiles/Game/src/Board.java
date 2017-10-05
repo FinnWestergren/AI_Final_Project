a513d4c4d31606b6ddc0f88008a9cf46254f753c
FinnWestergren <fwesterg@skidmore.edu>
2017-10-04 20:54:48 -0400

    getPossibleMoves, UndoMoves
    also initialized static array of all wall moves

--- JavaFiles/Game/src/Board.java
+++ JavaFiles/Game/src/Board.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Board implements BoardFeatures {
 
 	public int boardSize; // size of one side
@@ -6,6 +8,7 @@ public class Board implements BoardFeatures {
 	public Move[] allJuncMoves;
 	public GamePiece[] pieces = { new GamePiece(), new GamePiece() };
 	public int[] wallsLeft = { 10, 10 };
+	public static ArrayList<WallMove> allWallMoves = new ArrayList<WallMove>();
 
 	public Board(int boardSize) {
 		this.boardSize = boardSize;
@@ -23,10 +26,13 @@ public class Board implements BoardFeatures {
 
 				// init cells
 				cellArray[i][j] = new Cell();
-				
+
 				// init junctions
-				if (j < boardSize - 1 && i < boardSize - 1)
+				if (j < boardSize - 1 && i < boardSize - 1) {
 					junctArray[i][j] = new Junction();
+					allWallMoves.add(new WallMove(i, j, Orientation.HORIZONTAL));
+					allWallMoves.add(new WallMove(i, j, Orientation.VERTICAL));
+				}
 
 			}
 		}
@@ -42,7 +48,6 @@ public class Board implements BoardFeatures {
 		pieces[pID].setCol(x);
 		pieces[pID].setRow(y);
 	}
-
 
 	public void addWall(int x, int y, Orientation O, int pID) {
 		wallsLeft[pID]--;
@@ -61,8 +66,6 @@ public class Board implements BoardFeatures {
 				junctArray[x][y - 1].vertBlocked = true;
 		}
 	}
-
-
 
 	// for use with GameUI that will process graphics
 	public String toString() {
@@ -87,86 +90,124 @@ public class Board implements BoardFeatures {
 	 * 
 	 */
 
-	
-	
-
-	public Move[] getAllMoves(int pID) {
-		// TODO
-		return null;
+	public ArrayList<Move> getAllMoves(int pID) {
+		ArrayList<Move> mList = new ArrayList<Move>();
+		if (wallsLeft[pID] != 0)
+			mList.addAll(allWallMoves);
+		for (Direction dir : Direction.values()) {
+			mList.add(new PieceMove(dir));
+		}
+		for (Move i : getAllMoves(pID)) {
+			if (checkPossible(i, pID))
+				mList.add(i);
+		}
+		return mList;
 	}
 
 	@Override
-	public Move[] getPossibleMoves(int pID) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Move> getPossibleMoves(int pID) {
+		ArrayList<Move> mList = new ArrayList<Move>();
+		for (Move i : getAllMoves(pID)) {
+			if (checkPossible(i, pID))
+				mList.add(i);
+		}
+		return mList;
 	}
 
 	@Override
 	public void performMove(Move m, int pID) {
 		// TODO Auto-generated method stub
-		if (m instanceof PieceMove) 
+		if (m instanceof PieceMove)
 			performPieceMove((PieceMove) m, pID);
-		if (m instanceof WallMove) 
+		if (m instanceof WallMove)
 			performWallMove((WallMove) m, pID);
 	}
-	
+
 	private void performPieceMove(PieceMove m, int pID) {
 		int x = pieces[pID].getCol();
 		int y = pieces[pID].getRow();
-		
+
 		if ((m).getDirection() == Direction.UP) {
-			pieces[pID].setCol(x+1);
-			pieces[pID].setRow(y);
-			cellArray[x+1][y].occupied = true;
+			pieces[pID].setCol(x);
+			pieces[pID].setRow(y + 1);
+			cellArray[x + 1][y].occupied = true;
 		}
 		if ((m).getDirection() == Direction.DOWN) {
-			pieces[pID].setCol(x-1);
-			pieces[pID].setRow(y);
-			cellArray[x-1][y].occupied = true;
+			pieces[pID].setCol(x);
+			pieces[pID].setRow(y - 1);
+			cellArray[x - 1][y].occupied = true;
 		}
 		if ((m).getDirection() == Direction.LEFT) {
-			pieces[pID].setCol(x);
-			pieces[pID].setRow(y-1);
-			cellArray[x][y-1].occupied = true;
+			pieces[pID].setCol(x - 1);
+			pieces[pID].setRow(y);
+			cellArray[x][y - 1].occupied = true;
 		}
 		if ((m).getDirection() == Direction.RIGHT) {
-			pieces[pID].setCol(x);
-			pieces[pID].setRow(y+1);
-			cellArray[x][y+1].occupied = true;
+			pieces[pID].setCol(x + 1);
+			pieces[pID].setRow(y);
+			cellArray[x][y + 1].occupied = true;
 		}
-		
+
 		cellArray[x][y].occupied = false;
 	}
 
 	private void performWallMove(WallMove m, int pID) {
 		wallsLeft[pID]--;
-		
+
 		int x = m.getJunctX();
 		int y = m.getJunctY();
 		Orientation O = m.getOrientation();
-		
+
 		junctArray[x][y].setOrientation(O);
-		if(O == Orientation.HORIZONTAL) {
-			if(x < boardSize - 1)	junctArray[x+1][y].horizBlocked = true;
-			if(x > 0)				junctArray[x-1][y].horizBlocked = true;
+		if (O == Orientation.HORIZONTAL) {
+			if (x < boardSize - 1)
+				junctArray[x + 1][y].horizBlocked = true;
+			if (x > 0)
+				junctArray[x - 1][y].horizBlocked = true;
 		}
-		
-		if(O == Orientation.VERTICAL) {
-			if(y < boardSize - 1) 	junctArray[x][y+1].vertBlocked = true;
-			if(y > 0) 				junctArray[x][y-1].vertBlocked = true;
+
+		if (O == Orientation.VERTICAL) {
+			if (y < boardSize - 1)
+				junctArray[x][y + 1].vertBlocked = true;
+			if (y > 0)
+				junctArray[x][y - 1].vertBlocked = true;
 		}
 	}
 
 	@Override
-	public void undoMove(Move m) {
-		// TODO Auto-generated method stub
-		
+	public void undoMove(Move m, int pID) {
+		if (m instanceof PieceMove)
+			undoPieceMove((PieceMove) m, pID);
+		if (m instanceof WallMove)
+			undoWallMove((WallMove) m, pID);
+	}
+
+	private void undoWallMove(WallMove m, int pID) {
+		junctArray[m.getJunctX()][m.getJunctY()].setOrientation(Orientation.OPEN);
+		wallsLeft[pID]++;
+	}
+
+	private void undoPieceMove(PieceMove m, int pID) {
+		switch (m.getDirection()) {
+		case UP:
+			performPieceMove(new PieceMove(Direction.DOWN), pID);
+			break;
+		case DOWN:
+			performPieceMove(new PieceMove(Direction.UP), pID);
+			break;
+		case LEFT:
+			performPieceMove(new PieceMove(Direction.RIGHT), pID);
+			break;
+		case RIGHT:
+			performPieceMove(new PieceMove(Direction.LEFT), pID);
+			break;
+		}
 	}
 
 	@Override
 	public double getBoardValue(int pID) {
-		// TODO Auto-generated method stub
-		
+		// TODO Return Heuristic Value of Board!
+
 		return 0;
 	}
 
@@ -176,24 +217,27 @@ public class Board implements BoardFeatures {
 		if (m instanceof PieceMove)
 			return checkPieceMove((PieceMove) m, pID);
 		if (m instanceof WallMove)
-			return checkWallMove ((WallMove) m, pID);
+			return checkWallMove((WallMove) m, pID);
 
 		return false;
 	}
 
 	private boolean checkWallMove(WallMove m, int pID) {
-		
-		return false;
+
+		int x = m.getJunctX();
+		int y = m.getJunctY();
+		Orientation o = m.getOrientation();
+
+		return (junctArray[x][y].checkPossible(o));
 	}
 
 	@Override
 	public boolean checkGameOver() {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
-	
-	//returns whether piece move works, true for possible
+	// returns whether piece move works, true for possible
 	public boolean checkPieceMove(PieceMove m, int pID) {
 
 		int x1 = pieces[pID].getCol();
@@ -201,46 +245,52 @@ public class Board implements BoardFeatures {
 
 		int x2 = x1;
 		int y2 = y1;
-		
+
 		Orientation junct1 = Orientation.OPEN;
 		Orientation junct2 = Orientation.OPEN;
-		
 
 		switch (m.getDirection()) {
 		case UP:
 			y2--;
-			junct1 = getJunctionState(x1-1, y1-1);
-			junct2 = getJunctionState(x1, y1-1);
+			junct1 = getJunctionState(x1 - 1, y1 - 1);
+			junct2 = getJunctionState(x1, y1 - 1);
 			break;
 		case DOWN:
 			y2++;
-			junct1 = getJunctionState(x1-1, y1);
+			junct1 = getJunctionState(x1 - 1, y1);
 			junct2 = getJunctionState(x1, y1);
 			break;
 		case LEFT:
 			x2--;
-			junct1 = getJunctionState(x1-1, y1-1);
-			junct2 = getJunctionState(x1-1, y1);
+			junct1 = getJunctionState(x1 - 1, y1 - 1);
+			junct2 = getJunctionState(x1 - 1, y1);
 			break;
 		case RIGHT:
 			x2++;
-			junct1 = getJunctionState(x1, y1-1);
+			junct1 = getJunctionState(x1, y1 - 1);
 			junct2 = getJunctionState(x1, y1);
 			break;
 
 		default:
 			break;
 		}
-		
-		if(cellArray[x2][y2].occupied) return false;
-		
+
+		if (y2 == -1 && pID == 0)
+			return true;
+		if (y2 == boardSize && pID == 1)
+			return true;
+
+		if (cellArray[x2][y2].occupied)
+			return false;
 
 		return (junct1 == Orientation.OPEN && junct2 == Orientation.OPEN);
 	}
-	
+
 	public Orientation getJunctionState(int x, int y) {
-		if(x < 0 || x > boardSize - 1 || y < 0 || y > boardSize - 1) return Orientation.OPEN;
-		else return junctArray[x][y].getOrientation();
+		if (x < 0 || x > boardSize - 1 || y < 0 || y > boardSize - 1)
+			return Orientation.OPEN;
+		else
+			return junctArray[x][y].getOrientation();
 	}
 
 }
--- JavaFiles/Game/src/BoardFeatures.java
+++ JavaFiles/Game/src/BoardFeatures.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 
 public interface BoardFeatures {
 	//these are features that Board must implement in order to interact with players
 	
-	public Move[] getPossibleMoves(int pID);
+	public ArrayList<Move> getPossibleMoves(int pID);
 	public void performMove(Move m, int pID);
-	public void undoMove(Move m);
 	public double getBoardValue(int pID);
 	public boolean checkGameOver();
+	public void undoMove(Move m, int pID);
 }
