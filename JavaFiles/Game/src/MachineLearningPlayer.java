c6be1cd502175c45eec9eab2bc561cbe54ec93e1
skelderman <32072171+skelderman@users.noreply.github.com>
2017-11-27 23:06:59 -0500

    fixed my machine learner

--- JavaFiles/Game/src/MachineLearningPlayer.java
+++ JavaFiles/Game/src/MachineLearningPlayer.java
@@ -1,6 +1,3 @@
-
-public class MachineLearningPlayer {
-	
 	/*
 	 * Machine Learner Class
 	 * Implements Neural Net, Node, Synapse, Neural Layer...
@@ -18,20 +15,52 @@ public class MachineLearningPlayer {
 	 * 		
 	 * 	
 	 */
-	
-	private NeuralNet network;
-	
-	
-	
-	public MachineLearningPlayer(Board b, int pID, String Layout) {
-		//network = new NeuralNet()
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.io.PrintWriter;
+	import java.util.Scanner;
+
+	public class MachineLearningPlayer extends Player implements AI_Player {
+		
+		
+		private NeuralNet net;
+		private File weightsFile, layoutFile;
+
+		public MachineLearningPlayer( int pID) {
+			super(pID);
+		}
+		
+		public void init(Board b, File weightsFile, File layoutFile) {
+			this.weightsFile = weightsFile;
+			this.layoutFile = layoutFile;
+			net = new NeuralNet(b, pID, layoutFile, weightsFile);
+			printNetwork();
+		}
+		
+		public void printNetwork() {
+			String layout = net.layoutToString();
+			String weights = net.weightsToString();
+			try {
+				PrintWriter layoutPrinter = new PrintWriter(layoutFile);
+				layoutPrinter.write(layout);
+				layoutPrinter.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			try {
+				PrintWriter weightPrinter = new PrintWriter(weightsFile);
+				weightPrinter.write(weights);
+				weightPrinter.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		
+		
 	}
-	
-	public Move getMove(BoardFeatures board) {
-		
-		
-		
-		
-		return null;
-	}
-}
